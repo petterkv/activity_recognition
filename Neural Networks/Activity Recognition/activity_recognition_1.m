ecabef7e8035b6fc8693c591f24f24ab885e4a63
petterkv <61784716+petterkv@users.noreply.github.com>
2020-03-06 23:24:26 +0100

    Add files via upload

--- Neural Networks/Activity Recognition/activity_recognition_1.m
+++ Neural Networks/Activity Recognition/activity_recognition_1.m
@@ -1,24 +1,38 @@
 %Load data
-%[XTrain,YTrain] = japaneseVowelsTrainData;
-%[XValidation,YValidation] = japaneseVowelsTestData;
 ios_act_raw = readtable('ios_act.csv');
-head(ios_act_raw,5) % show first 5 rows of table
+
+%Remove unnecassary columns 
+ios_act = removevars(ios_act_raw, [1:4,12,13,17,21,25:47,49,50:54]);
+
+head(ios_act,5) % show first 5 rows of table
+whos %List variables in workspace
 
 %Massere data før oppdeling
 
-whos 
 rng(8000,'twister'); %Set the random number generator to a known state
-holdoutCVP = cvpartition(size(ios_act_raw,1),'holdout',0.2)
-idx = holdoutCVP.test;
+
+% Define a random partition on a set of data of a specified size
+iosactCVP = cvpartition(size(ios_act,1),'holdout',0.2) %holdout = test set
+idx = iosactCVP.test;
 
 %seperate to training and test data
-dataTrain = ios_act_raw(~idx,:);
-dataTest = ios_act_raw(idx,:);
+dataTrain = ios_act(~idx,:);
+dataTest = ios_act(idx,:);
 
-%XTrain(1:5)
+C = table2cell(dataTrain);
+C = ctranspose(C); %transpose the columns to rows
+dataTrainInput = [C{2,:}];
+
+%prepare data for input
+
+
 
 %Define the network architecture
-numFeatures = 57;
+numFeatures = 16;
+%The number of hidden units corresponds to the amount of information
+%remembered between time steps (the hidden state). The hidden state
+%can contain information from all previous time steps, regardless 
+%of the sequence length
 numHiddenUnits = 100;
 numClasses = 5;
 
@@ -41,8 +55,9 @@ options = trainingOptions('adam', ...
     'Shuffle','every-epoch', ...
     'Verbose',false, ...
     'Plots','training-progress');
+
 %Feiler her
-net = trainNetwork(dataTrain,layers,options);
+net = trainNetwork(C(:),layers,options);
 
 %Test the network
 YPred = classify(net,dataTest,'MiniBatchSize',miniBatchSize);
