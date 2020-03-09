ce1489348106ba5101d76e23a32e279d259d417f
petterkv <61784716+petterkv@users.noreply.github.com>
2020-03-09 23:04:07 +0100

    Add files via upload

--- Neural Networks/Activity Recognition/activity_recognition_1.m
+++ Neural Networks/Activity Recognition/activity_recognition_1.m
@@ -1,11 +1,12 @@
-%Load data
+%% Load data
 ios_act_raw = readtable('ios_act.csv');
-
 %Remove unnecassary columns 
 ios_act = removevars(ios_act_raw, [1:4,12,13,17,21,25:47,49,50:54]);
-
 head(ios_act,5) % show first 5 rows of table
+
+%% Prepare training and validation set
 whos %List variables in workspace
+
 
 %Massere data før oppdeling
 
@@ -18,13 +19,24 @@ idx = iosactCVP.test;
 %seperate to training and test data
 dataTrain = ios_act(~idx,:);
 dataTest = ios_act(idx,:);
+%% Transpose data
+%C = table2cell(dataTrain); %Convert to cell array
+%C = ctranspose(C); %transpose the columns to rows
+%Derive all the predictors for all the obseravtions. The predictors are
+%represented one one line after one orher 16 16 16 16 16
+%dataTrainInputX = [C{1:16,:}]; 
+%dataTrainInputX = tonndata([dataTrain{1:16,:}],true,false);
+%celldata = transpose(table2cell([dataTrain(:,1:16)]));
+%cell = table2cell(dataTrain(:,1:16));
+%trans = transpose(dataTrain(:,1:16));
 
-C = table2cell(dataTrain);
-C = ctranspose(C); %transpose the columns to rows
-dataTrainInput = [C{2,:}];
+%td = transpose(tonndata(dataTrain{1:16,:}, true, false));
+[predictors, wasMatrix] = tonndata(dataTrain{1:10,1:16},false,false);
+%dependentcategorical = ({transpose(categorical(dataTrain{1:10,17}))});
+[dependent, wasMatrix2] = tonndata(dataTrain{1:10,17},false,false);
+%[dependent, wasMatrix2] = tonndata(dataTrain{1:10,17},false,false);
 
-%prepare data for input
-
+%dataTrainInputX = transpose(tonndata(dataTrain{1:10,1:16}, false, false));
 
 
 %Define the network architecture
@@ -56,8 +68,8 @@ options = trainingOptions('adam', ...
     'Verbose',false, ...
     'Plots','training-progress');
 
-%Feiler her
-net = trainNetwork(C(:),layers,options);
+%% Feiler her
+net = trainNetwork(predictors, dependent, layers, options);
 
 %Test the network
 YPred = classify(net,dataTest,'MiniBatchSize',miniBatchSize);
